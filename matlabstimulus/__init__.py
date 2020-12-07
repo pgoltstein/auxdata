cea85347d1cc61e758f9c27386f4938777cac28e
Pieter Goltstein <pgoltstein@Pieters-MacBook-Pro.local>
2020-12-07 21:29:31 +0100

    added stimulus id functionality to the matlabstimulus module

--- matlabstimulus/__init__.py
+++ matlabstimulus/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from .matlabstimulus import stimulusinfo
+from .matlabstimulus import StimulusData
--- matlabstimulus/matlabstimulus.py
+++ matlabstimulus/matlabstimulus.py
@@ -13,6 +13,7 @@ Created on Thu May 7, 2020
 import os, glob
 import datetime
 import numpy as np
+from scipy.io import loadmat
 
 
 #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
@@ -41,8 +42,84 @@ class StimulusData(object):
         time_split = namesplitted[-2]
         self._datetime = datetime.datetime( 2000+int(date_split[0:2]), int(date_split[2:4]), int(date_split[4:6]), int(time_split[0:2]), int(time_split[2:4]), int(time_split[4:6]) )
 
+        # Load stimulus file
+        self._matfile = loadmat(self._stimfile)
 
     # properties
     def __str__(self):
         """ Returns a printable string with summary output """
         return "StimulusData file {} from {}\n* # of stimuli: {}".format( self._stimfilename, self._datetime, 0 )
+
+    @property
+    def stimulus(self):
+        """ Returns a list with id's of the stimuli """
+        return self._matfile['S']['StimIDs'][0,0].ravel()
+
+    @property
+    def stimulus_ix(self):
+        """ Returns a list with 'index' of the stimulus """
+        (_,stimulus_ix) = np.unique(self.stimulus,return_inverse=True)
+        return stimulus_ix
+
+    @property
+    def eye(self):
+        """ Returns a list with eye-id's of the stimuli """
+        return self._matfile['S']['EyeIDs'][0,0].ravel()
+
+    @property
+    def eye_ix(self):
+        """ Returns a list with 'index' of the eye """
+        (_,eye_ix) = np.unique(self.eye,return_inverse=True)
+        return eye_ix
+
+    @property
+    def direction(self):
+        """ Returns a list with directions of the stimuli """
+        direction_list = self._matfile['S']['Angles'][0,0].ravel()
+        return np.array(
+            [direction_list[s_ix] for s_ix in self.stimulus_ix])
+
+    @property
+    def direction_id(self):
+        """ Returns a list with the 'index' of stimulus direction """
+        (_,direction_id) = np.unique(self.direction,return_inverse=True)
+        return direction_id
+
+    @property
+    def spatialf(self):
+        """ Returns a list with spatial frequencies of the stimuli """
+        spatialf_list = self._matfile['S']['spatialF'][0,0].ravel()
+        return np.array(
+            [spatialf_list[s_ix] for s_ix in self.stimulus_ix])
+
+    @property
+    def spatialf_id(self):
+        """ Returns a list with the 'index' of stimulus spatial frequency """
+        (_,spatialf_id) = np.unique(self.spatialf,return_inverse=True)
+        return spatialf_id
+
+    @property
+    def azimuth(file):
+        """ Returns a list with azimuth of the stimuli """
+        azimuth_list = self._matfile['S']['Azimuth'][0,0].ravel()
+        return np.array(
+            [azimuth_list[s_ix] for s_ix in self.stimulus_ix])
+
+    @property
+    def azimuth_id(self):
+        """ Returns a list with the 'index' of stimulus azimuth """
+        (_,azimuth_id) = np.unique(self.azimuth,return_inverse=True)
+        return azimuth_id
+
+    @property
+    def elevation(file):
+        """ Returns a list with elevation of the stimuli """
+        elevation_list = self._matfile['S']['Elevation'][0,0].ravel()
+        return np.array(
+            [elevation_list[s_ix] for s_ix in self.stimulus_ix])
+
+    @property
+    def elevation_id(self):
+        """ Returns a list with the 'index' of stimulus elevation """
+        (_,elevation_id) = np.unique(self.elevation,return_inverse=True)
+        return elevation_id
