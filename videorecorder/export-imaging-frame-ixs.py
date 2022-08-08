f90907b1f3a56577ccae528f4549c1242922cfca
Pieter Goltstein <pgoltstein@Pieters-MacBook-Pro.local>
2022-08-08 12:11:46 +0200

    added option for  fus

--- videorecorder/export-imaging-frame-ixs.py
+++ videorecorder/export-imaging-frame-ixs.py
@@ -32,7 +32,7 @@ parser.add_argument('filepath', type=str, help= 'path to the folder holding the 
 parser.add_argument('-f', '--filestem', type=str, default="", help='filestem for selecting matching aux, lvd and eye files')
 parser.add_argument('-o', '--overwrite',  action="store_true", default=False, help='Enables to overwrite existing files')
 parser.add_argument('-m', '--matlab',  action="store_true", default=False, help='Stores data as .mat file')
-parser.add_argument('-fus', '--functionalultrasoundimaging',  action="store_true", default=False, help='Imaging triggers from functional ultrasound imaging setup (as opposed to two-photon imaging setup)')
+parser.add_argument('-f', '--fus',  action="store_true", default=False, help='Imaging triggers from functional ultrasound imaging setup (as opposed to two-photon imaging setup)')
 parser.add_argument('-n', '--nimagingplanes',  type=int, help='Number of imaging planes acquired in fast z-stack (default=1)', default=1)
 args = parser.parse_args()
 
@@ -44,7 +44,7 @@ filestem = args.filestem
 overwrite_old_files = args.overwrite
 store_as_matlab = args.matlab
 n_imaging_planes = args.nimagingplanes
-fusimaging = args.functionalultrasoundimaging
+fusimaging = args.fus
 
 # =============================================================================
 # Functions
