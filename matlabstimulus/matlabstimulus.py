d22615192aade2ecace640eba4a9d9b7552306a1
Pieter Goltstein <pgoltstein@Pieters-MacBook-Pro.local>
2023-01-13 13:29:32 +0100

    added 2ac to matlab stimulus

--- matlabstimulus/matlabstimulus.py
+++ matlabstimulus/matlabstimulus.py
@@ -23,7 +23,7 @@ class StimulusData(object):
     """ Loads and represents .mat stimulus data.
     """
 
-    def __init__(self, filepath=".", filename=None, gonogo=False):
+    def __init__(self, filepath=".", filename=None, gonogo=False, task=False):
         """ - filepath: Path to where the .mat file is located
             - filename: Optional exact filename
         """
@@ -35,7 +35,10 @@ class StimulusData(object):
         if filename is None: filename = "*StimSettings.mat"
         self._stimfile = glob.glob( os.path.join(filepath,filename) )[0]
         self._stimfilename = self._stimfile.split(os.path.sep)[-1]
-        self.gonogo = gonogo
+
+        # Set  stimulation metadata
+        self._gonogo = gonogo
+        self._task = task
 
         # Get date and time of the stimfile
         namesplitted = self._stimfilename.split("-")
@@ -49,7 +52,36 @@ class StimulusData(object):
     # properties
     def __str__(self):
         """ Returns a printable string with summary output """
-        return "StimulusData file {} from {}\n* Stim. dur={}s, iti={}s".format( self._stimfilename, self._datetime, self.stimulus_duration, self.iti_duration )
+        if self.task:
+            if self.gonogo:
+                stimulustype = "Go/nogo task"
+            else:
+                stimulustype = "2AC task"
+        else:
+            stimulustype = "Passive stimulation"
+        return "StimulusData file {} from {}\n* {}, stim. dur={}s, iti={}s".format( self._stimfilename, self.datetime, stimulustype, self.stimulus_duration, self.iti_duration )
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Stimulus metadata
+
+    @property
+    def task(self):
+        """ Returns True when task, False for passive stimulation """
+        return self._task
+
+    @property
+    def gonogo(self):
+        """ Returns True when task is  go/nogo, False for 2AC """
+        return self._gonogo
+
+    @property
+    def datetime(self):
+        """ Returns date and time """
+        return self._datetime
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Stimulus parameters
 
     @property
     def stimulus_duration(self):
@@ -66,64 +98,98 @@ class StimulusData(object):
         """ Returns the preset duration of the ITI """
         return float(self._matfile['S']['ITI'][0,0])
 
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Task parameters
+
+    @property
+    def response(self):
+        """ Returns a list with id's of the responses
+            Go/nogo: 1=go, 0=nogo
+            2AC: 0=missed trial, 1=left, 2=right
+        """
+        if self.task:
+            if self.gonogo:
+                return self._matfile['MousesResponse'].ravel()
+            else:
+                return self._matfile['ResponseSide'].ravel()
+        else:
+            return None
+
+    @property
+    def outcome(self):
+        """ Returns a list with id's of the outcome
+            Go/nogo: 0=incorrect, 1=correct
+            2AC: NaN= missed trial, 0=incorrect, 1=correct
+        """
+        if self.task:
+            return self._matfile['Outcome'].ravel()
+        else:
+            return None
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Stimulus identification
+
     @property
     def stimulus(self):
         """ Returns a list with id's of the stimuli """
-        if self.gonogo:
+        if self.task:
             return self._matfile['StimulusId'].ravel()
         else:
             return self._matfile['S']['StimIDs'][0,0].ravel()
 
     @property
     def stimulus_ix(self):
-        """ Returns a list with 'index' of the stimulus """
+        """ Returns a list with the 'zero based index' of each stimulus """
         (_,stimulus_ix) = np.unique(self.stimulus,return_inverse=True)
         return stimulus_ix
 
     @property
-    def response(self):
-        """ Returns a list with id's of the responses, 1=go, 0=nogo """
-        if self.gonogo:
-            return self._matfile['MousesResponse'].ravel()
-
-    @property
-    def outcome(self):
-        """ Returns a list with id's of the outcome, 1=correct, 2=incorrect """
-        if self.gonogo:
-            return self._matfile['Outcome'].ravel()
-
-    @property
     def category(self):
-        """ Returns a list with id's of the category, 2=go, 1=nogo"""
-        if self.gonogo:
+        """ Returns a list with id's of the category
+            Go/nogo: 1=nogo, 2=go
+            2AC: 1=left, 2=right
+        """
+        if self.task:
             return self._matfile['CategoryId'].ravel()
 
     @property
     def category_ix(self):
-        """ Returns a list with 'index' of the category, 2=go, 1=nogo """
+        """ Returns a list with 'index' of the category
+            Go/nogo: 0=nogo, 1=go
+            2AC: 0=left, 1=right
+        """
         (_,category_ix) = np.unique(self.category,return_inverse=True)
         return category_ix
 
     @property
     def eye(self):
         """ Returns a list with eye-id's of the stimuli """
-        return self._matfile['S']['EyeIDs'][0,0].ravel()
+        if self.task:
+            return None:
+        else:
+            return self._matfile['S']['EyeIDs'][0,0].ravel()
 
     @property
     def eye_ix(self):
         """ Returns a list with 'index' of the eye """
-        (_,eye_ix) = np.unique(self.eye,return_inverse=True)
-        return eye_ix
+        if self.task:
+            return None:
+        else:
+            (_,eye_ix) = np.unique(self.eye,return_inverse=True)
+            return eye_ix
 
     @property
     def direction(self):
-        """ Returns a list with directions of the stimuli """
-        if self.gonogo:
-            print()
-            direction_list = [self._matfile['S']['Cat1'][0,0]['Angles'][0,0].ravel(), self._matfile['S']['Cat2'][0,0]['Angles'][0,0].ravel()]
+        """ Returns a list with directions of the stimuli (on range of 0 to 360 degrees) """
+        if self.task:
+            if self.gonogo:
+                direction_list = [self._matfile['S']['Cat1'][0,0]['Angles'][0,0].ravel(), self._matfile['S']['Cat2'][0,0]['Angles'][0,0].ravel()]
+            else:
+                direction_list = [self._matfile['S']['LeftCat'][0,0]['Angles'][0,0].ravel(), self._matfile['S']['RightCat'][0,0]['Angles'][0,0].ravel()]
             return np.array(
                 [direction_list[c][s] for c,s in zip(self.category_ix,self.stimulus_ix)] )
-
         else:
             direction_list = self._matfile['S']['Angles'][0,0].ravel()
             return np.array(
@@ -138,11 +204,13 @@ class StimulusData(object):
     @property
     def spatialf(self):
         """ Returns a list with spatial frequencies of the stimuli """
-        if self.gonogo:
-            spatialf_list = [self._matfile['S']['Cat1'][0,0]['spatialF'][0,0].ravel(), self._matfile['S']['Cat2'][0,0]['spatialF'][0,0].ravel()]
+        if self.task:
+            if self.gonogo:
+                spatialf_list = [self._matfile['S']['Cat1'][0,0]['spatialF'][0,0].ravel(), self._matfile['S']['Cat2'][0,0]['spatialF'][0,0].ravel()]
+            else:
+                spatialf_list = [self._matfile['S']['LeftCat'][0,0]['spatialF'][0,0].ravel(),self._matfile['S']['RightCat'][0,0]['spatialF'][0,0].ravel()]
             return np.array(
                 [spatialf_list[c][s] for c,s in zip(self.category_ix,self.stimulus_ix)] )
-
         else:
             spatialf_list = self._matfile['S']['spatialF'][0,0].ravel()
             return np.array(
@@ -157,9 +225,17 @@ class StimulusData(object):
     @property
     def azimuth(self):
         """ Returns a list with azimuth of the stimuli """
-        azimuth_list = self._matfile['S']['Azimuth'][0,0].ravel()
-        return np.array(
-            [azimuth_list[s_ix] for s_ix in self.stimulus_ix])
+        if self.task:
+            if self.gonogo:
+                spatialf_list = [self._matfile['S']['Cat1'][0,0]['Azimuth'][0,0].ravel(), self._matfile['S']['Cat2'][0,0]['Azimuth'][0,0].ravel()]
+            else:
+                spatialf_list = [self._matfile['S']['LeftCat'][0,0]['Azimuth'][0,0].ravel(),self._matfile['S']['RightCat'][0,0]['Azimuth'][0,0].ravel()]
+            return np.array(
+                [spatialf_list[c][s] for c,s in zip(self.category_ix,self.stimulus_ix)] )
+        else:
+            azimuth_list = self._matfile['S']['Azimuth'][0,0].ravel()
+            return np.array(
+                [azimuth_list[s_ix] for s_ix in self.stimulus_ix])
 
     @property
     def azimuth_id(self):
@@ -170,9 +246,17 @@ class StimulusData(object):
     @property
     def elevation(self):
         """ Returns a list with elevation of the stimuli """
-        elevation_list = self._matfile['S']['Elevation'][0,0].ravel()
-        return np.array(
-            [elevation_list[s_ix] for s_ix in self.stimulus_ix])
+        if self.task:
+            if self.gonogo:
+                spatialf_list = [self._matfile['S']['Cat1'][0,0]['Elevation'][0,0].ravel(), self._matfile['S']['Cat2'][0,0]['Elevation'][0,0].ravel()]
+            else:
+                spatialf_list = [self._matfile['S']['LeftCat'][0,0]['Elevation'][0,0].ravel(),self._matfile['S']['RightCat'][0,0]['Elevation'][0,0].ravel()]
+            return np.array(
+                [spatialf_list[c][s] for c,s in zip(self.category_ix,self.stimulus_ix)] )
+        else:
+            elevation_list = self._matfile['S']['Elevation'][0,0].ravel()
+            return np.array(
+                [elevation_list[s_ix] for s_ix in self.stimulus_ix])
 
     @property
     def elevation_id(self):
